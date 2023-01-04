72bf0fc14115cc103f5fc83fefa01ec59bcf594a
1MarcosAquino <1marcosaquino@gmail.com>
2023-01-04 17:59:10 -0300

    test

---
+++ projeto-FrontEnd/pages/src/scripts/genericFunctions.js
@@ -0,0 +1,10 @@
+const selector = {
+  $All: (param) => document.querySelectorAll(param),
+  $One: (param) => document.querySelector(param),
+};
+
+function $Creator(param) {
+  return document.createElement(param);
+}
+
+export { selector, $Creator };
